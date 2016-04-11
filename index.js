eb66f52fe870804952841b3eb82f64734a3e2111
qiyu.pqy <qiyu.pqy@cainiao.com>
2016-04-11 15:54:52 +0800

    支持Promise

--- index.js
+++ index.js
@@ -6,11 +6,19 @@ module = module.exports = function(fn) {
     "__express": function(templatePath, options, callback) {
       templatePath = path.resolve(__dirname, templatePath);
       fs.readFile(templatePath, 'utf8', function(err, data) {
+        var output;
         if (err) {
           return callback(err, null);
         }
-        var output = fn.apply(this, [data, options]);
-        callback(null, output);
+        output = fn.apply(this, [data, options]);
+        if (output.then) {
+          output.then(function(data) {
+            callback(null, data);
+          }, function(err) {
+            callback(err, null);
+          })
+        } else
+          callback(null, output);
       });
     }
   };
